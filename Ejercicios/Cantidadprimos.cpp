cd12755e79117b64e81493bf3c598bf030c5c272
Maria Elizabeth Mesa Pineda <mmesap@darkstar.org>
2019-09-27 08:55:13 -0500

    nicsafhos

---
+++ Ejercicios/Cantidadprimos.cpp
@@ -0,0 +1,48 @@
+      
+	  
+#include <iostream>
+#include <cmath>
+
+int isprime(int m); 
+int num_primes(int m); 
+void print_primes(int m, int n);
+
+int main(void)
+{
+  print_primes(3, 1000);
+  return 0;
+}
+
+int isprime(int m)
+{
+  if (m == 1) return 0;
+  int flag = 1;
+  for(int ii = 2; ii <= std::sqrt(m); ++ii) {
+    if (m%ii==0) {
+      flag = 0;
+      break;
+    }
+  }
+  return flag;
+}
+
+int num_primes(int m)
+{
+  int sum = 0;
+  for(int ii = 2; ii <= m; ++ii) {
+    if (isprime(ii)==1) {
+      sum += 1;
+    }
+  }
+  return sum;
+}
+
+void print_primes(int m, int n)
+{
+  for(int ii = m; ii <= n; ++ii) {
+    std::cout << ii
+	      << "    " << num_primes(ii)
+	      <<  "    " << ii/(std::log(ii)-1.08366)
+	      << "\n";
+  }
+}
